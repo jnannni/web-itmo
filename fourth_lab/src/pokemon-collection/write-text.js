f1a55bf8903c0382d1c4d29c625e63ac06923725
jnannni <catya.ladokhina@gmail.com>
2020-04-28 10:03:26 +0300

    fix little problems

--- fourth_lab/src/pokemon-collection/write-text.js
+++
@@ -1,8 +0,0 @@
-/**
- * We pass the actual <p></p> tag by searching it with
- * document.querySelector
- * @param {} p
- */
- module.exports = (p) => {
-   p.innerHTML = 'Hello! From hidden text heeh';
-}
